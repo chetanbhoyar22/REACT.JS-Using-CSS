0da0c56c7a74fec1205e84287dc6108fd862bbac
chetanbhoyar22 <cab9722@gmail.com>
2021-02-03 23:05:33 +0530

    REACT.JS-Using-CSS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './Chetan.css';
+
+function App() {
+  return (
+    <div>
+      <h1>CSS Types -</h1>
+      <h2>1. By Tag (CSS)</h2>
+      <h2 className='wisdom'>2. By Class (CSS)</h2>
+      <p id='my_p_tag'>3. By ID (CSS)</p>
+    </div>
+  );
+}
+
+export default App;
