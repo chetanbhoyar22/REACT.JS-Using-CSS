c325d15858a2d909a974ad41879af53c0d73545e
Chetan Bhoyar <78400497+chetanbhoyar22@users.noreply.github.com>
2021-02-07 23:43:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './Chetan.css';
 function App() {
   return (
     <div>
-      <h1>CSS Types -</h1>
+      <h1>CSS Types - External</h1>
       <h2>1. By Tag (CSS)</h2>
       <h2 className='wisdom'>2. By Class (CSS)</h2>
       <p id='my_p_tag'>3. By ID (CSS)</p>
